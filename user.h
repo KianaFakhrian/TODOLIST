36b12321c1ee0286213466287d8fd992a4f374e2
KianaFakhrian <kianafakhrian1384@gmail.com>
2024-06-24 08:48:29 +0330

    user class file added

--- user.h
+++ user.h
@@ -1,11 +1,25 @@
 #ifndef USER_H
 #define USER_H
-
+#include "linkedlist.h"
 
 class User
 {
+
+public:
+    QString username ;
+    QString password ;
+    linkedList Tasks ;
 public:
     User();
+public:
+    void loadDate(const QStringList& data )
+    {
+        if(data.size() >= 2)
+        {
+            username = data.at(0) ;
+            password = data.at(1) ;
+        }
+    }
 };
 
 #endif // USER_H
