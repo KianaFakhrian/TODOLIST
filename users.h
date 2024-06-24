e9540bb0121cc2b60902680576f3f85f4f29c47b
KianaFakhrian <kianafakhrian1384@gmail.com>
2024-06-24 08:26:09 +0330

    tasks linkedlist added to user class

--- users.h
+++ users.h
@@ -1,4 +1,23 @@
 #ifndef USERS_H
 #define USERS_H
+#include <QString>
+#include <QStringList>
+#include "linkedlist.h"
+#include "task.h"
 
 #endif // USERS_H
+class Users{
+public:
+    QString username ;
+    QString password ;
+    linkedList Tasks ;
+public:
+    void loadDate(const QStringList& data )
+    {
+        if(data.size() >= 2)
+        {
+            username = data.at(0) ;
+            password = data.at(1) ;
+        }
+    }
+};
