fab6e5779581b3835fe3305d1ace866264907c38
KianaFakhrian <kianafakhrian1384@gmail.com>
2024-06-24 08:26:41 +0330

    loadData function added

--- task.h
+++ task.h
@@ -1,6 +1,7 @@
 #ifndef TASK_H
 #define TASK_H
 #include <QString>
+#include <QStringList>
 
 class Task
 {
@@ -8,19 +9,27 @@ public:
     QString Task_Name ;
     QString Due_Date ;
     QString Explanation ;
-    bool Star;
+    QString Star;
     Task* next ;
 public:
     Task();
     void set_task_info(Task New_Task)
     {
-
         Task_Name = New_Task.Task_Name ;
         Due_Date = New_Task.Due_Date ;
         Explanation = New_Task.Explanation ;
         Star = New_Task.Star ;
     }
-
+    void loadData(const QStringList& data)
+    {
+        if(data.size() >= 4)
+        {
+            Task_Name = data.at(0) ;
+            Due_Date = data.at(1) ;
+            Explanation = data.at(2) ;
+            Star = data.at(3) ;
+        }
+    }
 };
 
 #endif // TASK_H
