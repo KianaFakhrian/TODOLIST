56aa02852af5134de5264cc8149d734c38a091f1
KianaFakhrian <kianafakhrian1384@gmail.com>
2024-06-24 08:47:19 +0330

    making functions static

--- file_functions.h
+++ file_functions.h
@@ -2,15 +2,31 @@
 #define FILE_FUNCTIONS_H
 #include "task.h"
 #include "linkedlist.h"
-#include "users.h"
+#include "user.h"
 #include <QList>
+#include <QDebug>
 
 class File_Functions
 {
 public:
     File_Functions();
 public:
-    void read_Users_From_File(const QString& filePath ,QList<Users>& users)
+    static void write_Users_To_File(const QString& filePath ,User& users)
+    {
+        QFile file(filePath);
+
+        if (!file.open(QFile::Append | QFile::Text)) {
+            qWarning() << "Cannot open file for writing:" << file.errorString();
+            return;
+        }
+        QTextStream out(&file) ;
+
+            out << users.username << " " << users.password << "\n" ;
+
+        file.close();
+    }
+
+    static void read_Users_From_File(const QString& filePath ,QList<User>& users)
     {
         QFile file(filePath) ;
         if(!file.open(QFile::ReadOnly | QFile::Text))
@@ -24,14 +40,14 @@ public:
             QString line = in.readLine();
             QStringList data = line.split(' ');
 
-            Users tmp_user ;
-            tmp_user.loadData(data) ;
-            users.pushFront(tmp_user);
+            User tmp_user ;
+            tmp_user.loadDate(data);
+            users.append(tmp_user);
         }
         file.close();
     }
 
-    void write_Tasks_To_File(const QString&filePath , linkedList tasks)
+    static void write_Tasks_To_File(const QString&filePath , Task tasks,QString username)
     {
         QFile file(filePath);
 
@@ -41,11 +57,40 @@ public:
         }
         QTextStream out(&file) ;
 
-            out << tasks.Task_Name << " " << tasks.Due_Date << " "
-                << tasks.Explanation << "/n" ;
+            out <<username << " " <<tasks.Task_Name << " " << tasks.Due_Date << " "
+                << tasks.Explanation << " "<< tasks.Star <<"/n" ;
 
         file.close();
     }
+    static void read_Tasks_From_File(const QString& filepath , QList<User>& users)
+    {
+        QFile file(filepath) ;
 
+        if(!file.open(QFile::ReadOnly | QFile::Text))
+        {
+            qWarning() << "file couldn't be open" << file.errorString() ;
+            return ;
+        }
+        QTextStream in(&file) ;
+        while(!in.atEnd())
+        {
+            QString line = in.readLine() ;
+            QStringList data = line.split(' ') ;
+
+            if(data.size() >= 5)
+            {
+                QString username = data.at(0) ;
+                Task task ;
+                task.loadData(data.mid(1));
+
+                for(User& Users : users)
+                {
+                    if(Users.username == username)
+                        Users.Tasks.pushFront(task);
+                }
+            }
+        }
+        file.close();
+    }
 };
 #endif // FILE_FUNCTIONS_H
