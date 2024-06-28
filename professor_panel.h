7e13038468871d815dd6c706fc8e2faf128f8497
KianaFakhrian <kianafakhrian1384@gmail.com>
2024-06-28 14:33:16 +0330

    functions to load, add and update tasks and lists added

--- professor_panel.h
+++ professor_panel.h
@@ -3,7 +3,11 @@
 
 #include <QMainWindow>
 #include "user.h"
-
+#include "linkedlist.h"
+#include <QListWidgetItem>
+#include "file_functions.h"
+#include <QMap>
+#include <QMenu>
 namespace Ui {
 class Professor_Panel;
 }
@@ -14,13 +18,40 @@ class Professor_Panel : public QMainWindow
 
 public:
     explicit Professor_Panel(QWidget *parent = 0);
-    ~Professor_Panel();
+    virtual ~Professor_Panel();
 public:
     void get_Users(QList<User>* users_List) ;
+    void add_Task() ;
+    void add_list() ;
+//    void delete_tasks(QListWidgetItem*) ;
+    void set_User(User*) ;
+    void load_list() ;
+    void load_tasks() ;
+    void search_user() ;
+
+
+
+private slots:
+    void on_add_task_button_clicked();
+
+    void on_new_list_Button_clicked();
+
+    void switch_list(QListWidgetItem* item) ;
+
+//    void on_listWidget_itemClicked(QListWidgetItem *item);
+
+    void update_tasks(QListWidgetItem*) ;
+
+    void star_task(QListWidgetItem*) ;
 
 private:
     Ui::Professor_Panel *ui;
+    QString task_name ;
+    QString due_date ;
+    User* user ;
     QList<User>* users ;
+    QString currentList;
+    QMenu *taskListMenu;
 };
 
 #endif // PROFESSOR_PANEL_H
