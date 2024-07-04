aa7a2b65906ae647c4606e1c891f8c1856b55194
KianaFakhrian <kianafakhrian1384@gmail.com>
2024-07-04 12:35:23 +0330

    functions to updated file and linkedlist added

--- notification.h
+++ notification.h
@@ -2,6 +2,8 @@
 #define NOTIFICATION_H
 
 #include <QDialog>
+#include "user.h"
+#include "task.h"
 
 namespace Ui {
 class Notification;
@@ -14,9 +16,27 @@ class Notification : public QDialog
 public:
     explicit Notification(QWidget *parent = 0);
     ~Notification();
+public:
+    void get_user(User*) ;
+    void get_list_name(QString) ;
+    void get_task_name(QString) ;
+    void get_users(QList<User>*) ;
+    void update_file() ;
+private slots:
+    void on_Yes_clicked();
 
+    void on_No_clicked();
+
+    void search_Task() ;
+signals :
+    void exit() ;
 private:
     Ui::Notification *ui;
+    User* user ;
+    QList<User>* users ;
+    Task* task ;
+    QString list_name ;
+    QString task_name ;
 };
 
 #endif // NOTIFICATION_H
